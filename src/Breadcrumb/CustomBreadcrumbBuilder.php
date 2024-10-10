a055721960693d1d4c85d18d4ebbd3de4ecf4b34
pautri <Paul.Trilsbeek@mpi.nl>
2024-10-10 16:19:22 +0200

    facet breadcrumb progress

--- src/Breadcrumb/CustomBreadcrumbBuilder.php
+++ src/Breadcrumb/CustomBreadcrumbBuilder.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Drupal\Core\Link;
 use Drupal\facets\FacetInterface;
 use Drupal\facets\Entity\Facet;
+use Drupal\node\Entity\Node;
 
 
 class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
@@ -112,10 +113,6 @@ class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
 
         $facet_source_id = 'search_api:views_page__solr_search_content__page_1';
 
-        /** @var \Drupal\facets\FacetSource\FacetSourcePluginBase $facet_source_plugin */
-
-        $facet_source_plugin = $facet_source_manager->createInstance($facet_source_id);
-
         $source_id = str_replace(':', '__', $facet_source_id);
         /** @var \Drupal\facets\FacetSourceInterface $facet_source */
         $facet_source = $facet_source_storage->load($source_id);
@@ -123,7 +120,6 @@ class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
         // Add the required cacheability metadata.
         $breadcrumb->addCacheContexts(['url']);
         $breadcrumb->addCacheableDependency($facet_source);
-
 
         // Process the facets if they are not already processed.
         $facet_manager->processFacets($facet_source_id);
@@ -134,22 +130,16 @@ class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
             return (int) $a->getWeight() - $b->getWeight();
         });
 
-        /** @var \Drupal\facets\UrlProcessor\UrlProcessorPluginManager $url_processor_manager */
-        $url_processor_manager = \Drupal::service('plugin.manager.facets.url_processor');
-
         // Get active facets and results to use them at building the crumbs.
         $active_results = [];
-        $active_facets = [];
         foreach ($facets as $facet) {
             if (count($facet->getActiveItems()) > 0) {
                 // Add the facet as a cacheable dependency.
                 $breadcrumb->addCacheableDependency($facet);
 
-                /** @var \Drupal\facets\UrlProcessor\UrlProcessorInterface $url_processor */
-                $url_processor = $url_processor_manager->createInstance($facet_source->getUrlProcessorName(), ['facet' => $facet]);
                 $facet_manager->build($facet);
 
-                //dpm($facet->getResults());
+                $facet_id = $facet->id();
 
                 $results = $facet->getResults();
 
@@ -158,29 +148,42 @@ class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
                     $cloned_result = clone $result;
 
                     if (in_array($cloned_result->getRawValue(), $cloned_result->getFacet()->getActiveItems())) {
-                        $cloned_result->setActiveState(TRUE);
-                        $active_results[$facet->id()][] = $cloned_result;
 
-                        $active_facets[$facet->id()] = $facet;
+                        $active_results[$facet_id][] = $cloned_result;
+                    }
+
+                    if (str_contains($facet_id, '_exclude')) {
+
+                        $active_results[$facet_id][] = $cloned_result;
+
                     }
                 }
             }
         }
 
-        //dpm($active_results);
+        $all_facet_crumb_items = [];
 
         /** @var \Drupal\facets\Result\ResultInterface[] $facet_results */
         foreach ($active_results as $facet_id => $facet_results) {
+
             $facet_used_result[$facet_id] = [];
-            $facet_crumb_items = [];
 
             foreach ($facet_results as $res) {
-                //dpm($res->getRawValue());
-                $facet_used_result[$facet_id][] = $res->getRawValue();
-                $facet_crumb_items = $res->getFacet()->getActiveItems();
-            }
+                if (str_contains($facet_id, '_exclude')) {
+                    $active_items = $res->getFacet()->getActiveItems();
+                    foreach ($active_items as $active_item) {
 
-            sort($facet_crumb_items);
+                        $facet_used_result[$facet_id][] = $active_item;
+                    }
+                }
+                else {
+                    $facet_used_result[$facet_id][] = $res->getRawValue();
+                }
+                $all_facet_crumb_items[$facet_id] = $res->getFacet()->getActiveItems();
+            }
+        }
+
+        foreach ($all_facet_crumb_items as $facet_id => $facet_crumb_items) {
 
             foreach ($facet_crumb_items as $facet_crumb_item) {
 
@@ -197,12 +200,13 @@ class CustomBreadcrumbBuilder implements BreadcrumbBuilderInterface
                 }
 
                 $facet_url->setOptions($options);
-
-                if (!empty($facet_source->getBreadcrumbSettings()['before'])) {
-                    $crumb_text = $active_facets[$facet_id]->label() . ': ' . $facet_crumb_item;
+                if ($facet_id == 'descendant_of') {
+                    $nid = $facet_crumb_item;
+                    $node = Node::load($nid);
+                    $crumb_text = $node->getTitle();
                 } else {
                     $crumb_text = $facet_crumb_item;
-                };
+                }
                 $link = Link::fromTextAndUrl($crumb_text, $facet_url);
 
                 $breadcrumb->addLink($link);
